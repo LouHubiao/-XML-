f1b25a7f8710ca79d634b3cbfae1d820c4ec566d
ambleLou <912389750@qq.com>
2014-10-02 14:33:44 +0800

    增加了对于节点属性的操作

--- XMLAnalysis2/Program.cs
+++ XMLAnalysis2/Program.cs
@@ -9,7 +9,6 @@ namespace XMLAnalysis2
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             XMLAnalysisClass analysis = new XMLAnalysisClass();
@@ -18,18 +17,29 @@ namespace XMLAnalysis2
             analysis.AddNode(root, "column2", "hello");
             analysis.DeleteNode(root, "column1");
             analysis.UpdateNode(root, "column2", "lou");
+            analysis.AddProperty(root, "column2", "type", "INT");
+            analysis.UpdateProperty(root, "column2", "type", "VARCHAR");
+            analysis.DeleteProperty(root, "column2", "type");
             string findValue = analysis.SearchNode(root, "column2");
             Console.WriteLine(findValue);
-            analysis.CleanFile(root);
+            //analysis.CleanFile(root);
             analysis.document.Save("bookstore.xml");
         }
+
         class XMLAnalysisClass
         {
+            //
             public XmlDocument document;
+            //属性list，保存预设的属性名和属性值选项；
+            public PropertyList propertys;
+            
             public XMLAnalysisClass()
             {
                 document = new XmlDocument();
+                propertys = new PropertyList();
             }
+            
+            //打开xml文件，获取xml文件的根节点；
             public XmlNode ReadXMLFile(string filePath)
             {
                 document.Load(filePath);
@@ -37,6 +47,108 @@ namespace XMLAnalysis2
                 return root;
             }
 
+            #region 工具函数
+            //根据列名获取对应xml中的元素节点；
+            public XmlElement getElementFromColumName(XmlNode root, string theColumnName)
+            {
+                if (root.FirstChild != null) { }
+                XmlNodeList nodeList = root.ChildNodes;
+                foreach (XmlNode node in nodeList)
+                {
+                    XmlElement element = (XmlElement)node;
+                    if (element.Name == theColumnName)
+                    {
+                        return element;
+                    }
+                }
+                Console.WriteLine("no this column");
+                //exit(0);
+                return null;
+            }
+
+            //验证属性名是否合法
+            public bool IsPropertyLegal(string propertyName, string propertyValue)
+            {
+                foreach (Property property in propertys.propertyList)
+                {
+                    if (propertyName == property.propertyName)
+                    {
+                        foreach (string value in property.propertyValues)
+                        {
+                            if (propertyValue == value)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+            #endregion
+
+            #region 属性处理
+            //添加节点属性，包括类型，是否允许空，是否为主键
+            public void AddProperty(XmlNode root, string theColumnName, string propertyName, string propertyValue)
+            {
+                if (IsPropertyLegal(propertyName, propertyValue) == true)
+                {
+                    
+                    XmlElement element = getElementFromColumName(root, theColumnName);
+                    foreach (XmlAttribute attribute in element.Attributes)
+                    {
+                        if (attribute.Name == propertyName)
+                        {
+                            Console.WriteLine("AddProperty: the property name has exsit");
+                            return;
+                        }
+                    }
+                    element.SetAttribute(propertyName, propertyValue);
+                }
+                else
+                {
+                    Console.WriteLine("AddProperty:propertyName or propertyValue is illegal");
+                }
+            }
+
+            //修改属性值
+            public void UpdateProperty(XmlNode root, string theColumnName, string propertyName, string propertyValue)
+            {
+                if (IsPropertyLegal(propertyName, propertyValue) == true)
+                {
+                    XmlElement element = getElementFromColumName(root, theColumnName);
+                    if (element.Attributes[propertyName] != null)
+                    {
+                        element.Attributes[propertyName].InnerText = propertyValue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("UpdateProperty:no this property name");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("UpdateProperty:propertyName or propertyValue is illegal");
+                }
+            }
+
+            public void DeleteProperty(XmlNode root, string theColumnName, string propertyName)
+            {
+                XmlElement element = getElementFromColumName(root, theColumnName);
+                foreach (XmlAttribute attribute in element.Attributes)
+                {
+                    if (attribute.Name == propertyName)
+                    {
+                        element.RemoveAttribute(propertyName);
+                        return;
+                    }
+                }
+                Console.WriteLine("DeleteProperty: no this property");
+            }
+
+            #endregion
+
+            #region 增删改查
+            //添加节点的主干，参数节点值，后续添加节点属性；
             public void AddNode(XmlNode root, string theColumnName, string theValue)
             {
                 XmlElement column = document.CreateElement(theColumnName);
@@ -46,50 +158,56 @@ namespace XMLAnalysis2
                 root.AppendChild(column);
             }
 
+            //删除节点，参数列名；
             public void DeleteNode(XmlNode root, string theColumnName)
             {
-                XmlNodeList nodeList = root.ChildNodes;
-                foreach (XmlNode node in nodeList)
+                XmlElement element = getElementFromColumName(root, theColumnName);
+                XmlElement value = (XmlElement)element.FirstChild;
+                if (value.Name == "Value")
                 {
-                    XmlElement element = (XmlElement)node;
-                    if (element.Name == theColumnName)
-                    {
-                        XmlElement value = (XmlElement)element.FirstChild;
-                        if (value.InnerText != null)
-                            value.InnerText = null;
-                    }
+                    if (value.InnerText != null)
+                        value.InnerText = null;
+                }
+                else
+                {
+                    Console.WriteLine("DeleteNode:no the VALUE element");
                 }
             }
 
+            //更新节点，参数列名；
             public void UpdateNode(XmlNode root, string theColumnName, string theValue)
             {
-                XmlNodeList nodeList = root.ChildNodes;
-                foreach (XmlNode node in nodeList)
+                XmlElement element = getElementFromColumName(root, theColumnName);
+                if (element != null)
                 {
-                    XmlElement element = (XmlElement)node;
-                    if (element.Name == theColumnName)
-                    {
-                        XmlElement value = (XmlElement)element.FirstChild;
-                        value.InnerText = theValue;
-                    }
+                    XmlElement value = (XmlElement)element.FirstChild;
+                    value.InnerText = theValue;
+                }
+                else
+                {
+                    Console.WriteLine("UpdateNode:no this node");
                 }
             }
 
+            //查找值，参数列名；
             public string SearchNode(XmlNode root, string theColumnName)
             {
-                XmlNodeList nodeList = root.ChildNodes;
-                foreach (XmlNode node in nodeList)
+                XmlElement element = getElementFromColumName(root, theColumnName);
+                if (element != null)
                 {
-                    XmlElement element = (XmlElement)node;
-                    if (element.Name == theColumnName)
-                    {
-                        XmlElement value = (XmlElement)element.FirstChild;
-                        return value.InnerText;
-                    }
+                    XmlElement value = (XmlElement)element.FirstChild;
+                    return value.InnerText;
                 }
-                return null;
+                else
+                {
+                    Console.WriteLine("SearchNode:no this node");
+                    return null;
+                }
             }
-            
+
+            //清除xml文件中除了根节点以外的所有节点，用于测试；
+            #endregion
+
             public void CleanFile(XmlNode root)
             {
                 XmlElement rootElement = (XmlElement)root;
---
+++ XMLAnalysis2/Property.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLAnalysis2
+{
+    class PropertyList
+    {
+        public List<Property> propertyList;
+
+        public PropertyList()
+        {
+            string[] type = { "type", "INT", "VARCHAR", "DATE" };
+            string[] nullable = { "NULL", "NULL", "NOT NULL" };
+            List<string[]> propertys = new List<string[]>();
+            propertys.Add(type);
+            propertys.Add(nullable);
+            propertyList = new List<Property>();
+            foreach (string[] property in propertys) {
+                string propertyName = property[0];
+                List<string> propertyValues = new List<string>();
+                for (int i = 1; i < property.Length; i++)
+                {
+                    propertyValues.Add(property[i]);
+                }
+                Property newProperty = new Property(propertyName, propertyValues);
+                propertyList.Add(newProperty);
+            }
+        }
+    }
+
+    class Property
+    {
+        public string propertyName;
+        public List<string> propertyValues;
+
+        public Property(string propertyName, List<string> propertyValues) {
+            this.propertyName = propertyName;
+            this.propertyValues = propertyValues;
+        }
+    }
+
+}
